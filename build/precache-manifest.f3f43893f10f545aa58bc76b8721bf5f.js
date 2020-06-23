2b987711809a12545088258be5d7786446c4937a
prathamnishad <54236823+prathamnishad@users.noreply.github.com>
2020-06-23 10:25:22 +0530

    for slider component

---
+++ build/precache-manifest.f3f43893f10f545aa58bc76b8721bf5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "583940487efef7317e45e270b5bb3d00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ac86a5c3be7f40f6986",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "bfc6af42848f55909d72",
+    "url": "/static/css/main.97dd4690.chunk.css"
+  },
+  {
+    "revision": "1ac86a5c3be7f40f6986",
+    "url": "/static/js/2.b0b152d3.chunk.js"
+  },
+  {
+    "revision": "16b20f0d39da2178839d67e1fac1e563",
+    "url": "/static/js/2.b0b152d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc6af42848f55909d72",
+    "url": "/static/js/main.dd4d9a6e.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  }
+]);
